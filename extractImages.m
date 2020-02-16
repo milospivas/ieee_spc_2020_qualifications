54ebaafd04b710f7d89eb06bcfb27d833fa0889d
Miloš Pivaš <milos.pivas@gmail.com>
2020-02-16 19:21:53 +0100

    Minor fix. Now extractImagse now returns a cell array.

--- extractImages.m
+++ extractImages.m
@@ -1,14 +1,13 @@
 function images = extractImages(bag)
 %EXTRACTIMAGES Extracts images from bag file
 %   Forms a set of all images from one bag file
-bSel = select(bag,'Topic','/pylon_camera_node/image_raw');
-normal_images = readMessages(bSel,'DataFormat','struct');
-images = [];
-for i=1:length(normal_images)
-  image = vec2mat(normal_images{i,1}.Data,normal_images{i,1}.Width);
-  image = imrotate(image,180);
-  images = [images; {image}];
-end
+    bSel = select(bag,'Topic','/pylon_camera_node/image_raw');
+    images = readMessages(bSel); %,'DataFormat','struct');
+
+    for i=1:length(images)
+        images{i} = readImage(images{i});
+        images{i} = imrotate(images{i}, 180);
+    end
 
 end
 
